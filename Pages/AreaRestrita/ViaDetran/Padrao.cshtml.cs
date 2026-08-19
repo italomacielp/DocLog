99605b2d1b0075f4f9fda3df0b5adcea862b0601
beatriz.pegado <anabeatrizdasilvapegado@gmail.com>
2026-08-19 10:44:58 -0300

    refatora para projeto razorpages

---
+++ Pages/AreaRestrita/ViaDetran/Padrao.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DocLog.Views.Home.ViaDetran
+{
+    public class PadraoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
